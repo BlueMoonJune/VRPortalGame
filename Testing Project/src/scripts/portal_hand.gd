a9ec1e4b8252c88b095d9f2b6ec5f7e3a792327a
Ovionyx <45300174+Ovionyx@users.noreply.github.com>
2022-01-30 19:17:03 -0800

    fixed order of execution in portal_hand.gd

--- Testing Project/src/scripts/portal_hand.gd
+++ Testing Project/src/scripts/portal_hand.gd
@@ -12,12 +12,12 @@ func _ready():
 
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	if controller.is_button_pressed(2):
-		pressed2 = true
+	if controller.is_button_pressed(2) or Input.is_action_pressed("ui_accept"):
 		if !pressed2:
 			var orb : RigidBody = scene.instance()
 			orb.linear_velocity = global_transform.basis.z * -10.0
 			add_child(orb)
 			orb.global_transform = Transform(Basis(Vector3(1,0,0),Vector3(0,1,0),Vector3(0,0,1)),global_transform.origin)
+			pressed2 = true
 	else:
 		pressed2 = false
