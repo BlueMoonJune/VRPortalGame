8d7649d6a2a374b60633b61c47c25ab126aa090f
Ovionyx <45300174+Ovionyx@users.noreply.github.com>
2022-01-30 20:30:01 -0800

    aaaaaa

--- Testing Project/src/scripts/portal_hand.gd
+++ Testing Project/src/scripts/portal_hand.gd
@@ -1,22 +1,23 @@
 extends Spatial
 
 
-export var path = "res://src/scenes/orange_portal_orb.tscn"
-var scene = load(path)
+export var path = "res://src/projectiles/orange_portal_orb.tscn"
+var scene : Spatial = null
+var projectile
 var controller : ARVRController
 var pressed2 : bool = false
 
 func _ready():
+	projectile = load(path)
 	controller = get_parent()
+	scene = controller.get_tree().root.get_child(0)
 
-
-# Called when the node enters the scene tree for the first time.
 func _process(delta):
 	if controller.is_button_pressed(2) or Input.is_action_pressed("ui_accept"):
 		if !pressed2:
-			var orb : RigidBody = scene.instance()
+			var orb : RigidBody = projectile.instance()
 			orb.linear_velocity = global_transform.basis.z * -10.0
-			add_child(orb)
+			scene.add_child(orb)
 			orb.global_transform = Transform(Basis(Vector3(1,0,0),Vector3(0,1,0),Vector3(0,0,1)),global_transform.origin)
 			pressed2 = true
 	else:
