7a2eff5c8a211c070bc3fe4271eb026700459cd8
Ovionyx <45300174+Ovionyx@users.noreply.github.com>
2022-02-03 16:04:54 -0800

    Finger Animation

--- Skeleton Animator/handanim.gd
+++ Skeleton Animator/handanim.gd
@@ -4,9 +4,69 @@ var skel : Skeleton
 var time : float = 0
 const VZERO : Vector3 = Vector3.ZERO
 const VONE : Vector3 = Vector3(1,1,1)
-var fingers : Array = [0,0,0,0]
+var fingers : Array = [0,0,0,0,0]
 
+const fingerbind : Array =      [ 0, 0, 1, 1, 1, 0, 2, 2, 2, 0, 2, 2, 2, 0, 2, 2, 2 ]
+const secfingerbind : Array =   [ 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1 ]
+const secfingerweight : Array = [ 0, 0, 0, 0, 0, 0,.3,.3,.2, 0,.2,.2,.1, 0,.1,.1, 0 ]
 
+const fingersout : Array = [
+	VZERO,
+	VZERO,
+	Vector3(-5,0,0),
+	Vector3(-5,0,0),
+	Vector3(-5,0,0),
+	VZERO,
+	Vector3(-6,0,0),
+	Vector3(-6,0,0),
+	Vector3(-6,0,0),
+	VZERO,
+	Vector3(-8,0,0),
+	Vector3(-8,0,0),
+	Vector3(-8,0,0),
+	VZERO,
+	Vector3(-6,0,6),
+	Vector3(-6,0,0),
+	Vector3(-6,0,0)
+]
+const fingerstouch : Array = [
+	VZERO,
+	VZERO,
+	Vector3(-60,0,0),
+	Vector3(-60,0,0),
+	Vector3(-60,0,0),
+	VZERO,
+	Vector3(-60,0,0),
+	Vector3(-60,0,0),
+	Vector3(-60,0,0),
+	VZERO,
+	Vector3(-60,0,0),
+	Vector3(-60,0,0),
+	Vector3(-60,0,0),
+	VZERO,
+	Vector3(-60,0,0),
+	Vector3(-60,0,0),
+	Vector3(-60,0,0)
+]
+const fingersclench : Array = [
+	VZERO,
+	VZERO,
+	Vector3(-90,0,0),
+	Vector3(-90,0,0),
+	Vector3(-90,0,0),
+	VZERO,
+	Vector3(-90,0,0),
+	Vector3(-90,0,0),
+	Vector3(-90,0,0),
+	VZERO,
+	Vector3(-90,0,0),
+	Vector3(-90,0,0),
+	Vector3(-90,0,0),
+	VZERO,
+	Vector3(-90,0,0),
+	Vector3(-90,0,0),
+	Vector3(-90,0,0)
+]
 
 func transf(t_position : Vector3, t_rotation : Vector3, t_scale : Vector3):
 	
@@ -25,27 +85,26 @@ func oscil(t : float):
 	return abs(fmod(t,2)-1)
 
 func _ready():
-	print(transf(VZERO,Vector3(45,0,45),VONE))
 	skel = $Armature/Skeleton
 
 func _process(delta):
-   #lerp(-90,0,smoothstep(0,1,oscil(time)))
+   #lerp(0,-90,smoothstep(0,1,oscil(time)))
 
-	fingers[0] -= Input.get_axis("indexup","indexdown") * delta
-	fingers[1] -= Input.get_axis("middleup","middledown") * delta
-	fingers[2] -= Input.get_axis("ringup","ringdown") * delta
-	fingers[3] -= Input.get_axis("pinkyup","pinkydown") * delta
-
-	time += delta 
-	skel.set_bone_custom_pose(2,transf(VZERO,Vector3(lerp(-90,0,fingers[0]),0,0),VONE))
-	skel.set_bone_custom_pose(6,transf(VZERO,Vector3(lerp(-90,0,fingers[1]),0,0),VONE))
-	skel.set_bone_custom_pose(10,transf(VZERO,Vector3(lerp(-90,0,fingers[2]),0,0),VONE))
-	skel.set_bone_custom_pose(14,transf(VZERO,Vector3(lerp(-90,0,fingers[3]),0,0),VONE))
-	skel.set_bone_custom_pose(3,transf(VZERO,Vector3(lerp(-90,0,fingers[0]),0,0),VONE))
-	skel.set_bone_custom_pose(7,transf(VZERO,Vector3(lerp(-90,0,fingers[1]),0,0),VONE))
-	skel.set_bone_custom_pose(11,transf(VZERO,Vector3(lerp(-90,0,fingers[2]),0,0),VONE))
-	skel.set_bone_custom_pose(15,transf(VZERO,Vector3(lerp(-90,0,fingers[3]),0,0),VONE))
-	skel.set_bone_custom_pose(4,transf(VZERO,Vector3(lerp(-90,0,fingers[0]),0,0),VONE))
-	skel.set_bone_custom_pose(8,transf(VZERO,Vector3(lerp(-90,0,fingers[1]),0,0),VONE))
-	skel.set_bone_custom_pose(12,transf(VZERO,Vector3(lerp(-90,0,fingers[2]),0,0),VONE))
-	skel.set_bone_custom_pose(16,transf(VZERO,Vector3(lerp(-90,0,fingers[3]),0,0),VONE))
+	fingers[1] = move_toward(fingers[1],int(Input.is_action_pressed("indexclose")),delta*10)
+	fingers[2] = Input.get_action_strength("indexclench")
+	fingers[3] = move_toward(fingers[3],int(Input.is_action_pressed("middleclose")),delta*10)
+	fingers[4] = Input.get_action_strength("middleclench")
+	
+	for i in range(0,skel.get_bone_count()-4):
+		var bind = fingerbind[i]
+		var secbind = secfingerbind[i]
+		var weight = secfingerweight[i]
+		
+		var out = fingersout[i]
+		var touch = fingerstouch[i]
+		var clench = fingersclench[i]
+		var val1 = max(fingers[bind*2-1],fingers[secbind*2-1]*weight)
+		var pose = lerp(out,touch,val1)
+		var val2 = fingers[bind*2]
+		pose = lerp(pose,clench,val2)
+		skel.set_bone_custom_pose(i,transf(VZERO,pose,VONE))
